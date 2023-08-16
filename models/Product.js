fa57d721f8e0fb08867e52391b73ec63bec10cd1
vedant <ursvedantyetekar@gmail.com>
2023-08-16 22:26:13 +0530

    created end point for adding a product

---
+++ models/Product.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const ProductModel =
+  mongoose.models.Product || mongoose.model("Product", ProductSchema);
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,10 +1,14 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 function NewProduct() {
   const [title, setTitle] = useState("");
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const router = useRouter();
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -15,31 +19,43 @@ function NewProduct() {
   const handlePriceChange = (e) => {
     setPrice(e.target.value);
   };
+  const createProduct = async (e) => {
+    e.preventDefault();
+    const data = { price, title, description };
+    await axios.post("/api/products", data);
+    setGoToProducts(true);
+  };
+
+  if (goToProducts) {
+    router.push("/products");
+  }
 
   return (
     <Layout>
-      <h1>New Product</h1>
-      <label>Product name</label>
-      <input
-        value={title}
-        onChange={handleTitleChange}
-        type="text"
-        placeholder="product name"
-      />
-      <label>Description</label>
-      <textarea
-        onChange={handleDescriptionChange}
-        value={description}
-        placeholder="description"
-      />
-      <label>Price (in USD)</label>
-      <input
-        value={price}
-        onChange={handlePriceChange}
-        type="number"
-        placeholder="price"
-      />
-      <button className="btn-primary">Save</button>
+      <form onSubmit={createProduct}>
+        <h1>New Product</h1>
+        <label>Product name</label>
+        <input
+          value={title}
+          onChange={handleTitleChange}
+          type="text"
+          placeholder="product name"
+        />
+        <label>Description</label>
+        <textarea
+          onChange={handleDescriptionChange}
+          value={description}
+          placeholder="description"
+        />
+        <label>Price (in USD)</label>
+        <input
+          value={price}
+          onChange={handlePriceChange}
+          type="number"
+          placeholder="price"
+        />
+        <button className="btn-primary">Save</button>
+      </form>
     </Layout>
   );
 }
