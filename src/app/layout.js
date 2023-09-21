0bbfd3020c49e66d5fcdde1704684c667d5f6c16
vedant <ursvedantyetekar@gmail.com>
2023-09-21 23:17:01 +0530

    writtend media queries for smaller to larger screens

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "VedantSays: Code Your Life",
+  description: `Discover programming mastery and lifestyle insights at VedantSpeaks. Explore our blog for in-depth articles on MERN stack development, JavaScript tricks, competitive programming strategies, fashion tips, and health advice. Join us on this journey of learning, creating, and living your best life.`,
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div></div>;
 }
